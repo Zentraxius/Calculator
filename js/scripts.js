ddd957bc10a4909ff81f70b24ff2811b6668925e
Cody Fritz <clanalia55@gmail.com>
2020-06-03 14:06:25 -0700

    add user interface to enable convenient addition
    co-authored-by: Thomas Glenn <thomasgrantglen@mac.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 const add = function (number1, number2) {
   return number1 + number2;
 };
-const number1 = parseInt(prompt("Enter a number:"));
+// // // // //
+const subtract = function (number1, number2) {
+  return number1 - number2;
+};
+// // // // //
+const multiply = function (number1, number2) {
+  return number1 * number2;
+};
+// // // // //
+const divide = function (number1, number2) {
+  return number1 / number2;
+};
+//___________________________________//
+//  Business Logic above this line  //
+//_________________________________//
+//User Interface Logic begins here//
 
-const number2 = parseInt(prompt("Enter another number:"));
+// // // // //
 
-alert(add(number1, number2));
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
---
+++ readme.md
@@ -0,0 +1,32 @@
+# _{Calculator}_
+
+#### _{It calculates}, {6/3/20}_
+
+#### By _**{Cody Fritz and Thomas Glenn}**_
+
+## Description
+
+An incredibly powerful calculator, capable of processing computational functions as high as 99\*99
+
+## Setup/Installation Requirements
+
+Simply launch index.html with browser of choice after extracting folder to location of choice
+
+## Known Bugs
+
+Limited digit support
+
+## Support and contact details
+
+For questions please contact clanalia55@gmail.com
+
+## Technologies Used
+
+HTML, CSS, JavaScript
+Bootstrap / jQuery
+
+### License
+
+MIT
+
+Copyright (c) 2016 **_{Cody Fritz}_**
